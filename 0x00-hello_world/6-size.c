b1d0933ec4b06c29e58d04e943aab0cb79d14173
Isaac <igwenlewechi@gmail.com>
2023-02-16 13:20:22 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - port of entry check out was successfull
-* Return: Alright 0
+* main - entry
+* Return: Always 0
 */
 int main(void)
 {
 	printf("size of a char: %i byte(s)\n", sizeof(char));
-	printf("size of an int: %i byte(s)\n", sizeoff(int));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
